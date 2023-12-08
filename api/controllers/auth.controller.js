317f7efb8afaf0d26798644d5e56d66db2cf379b
Sandip Gangakhedkar <sandip.gangakhedkar@gmail.com>
2023-12-08 22:54:50 +0000

    Adding custom error handler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const login = async (req, res) => {
+export const login = async (req, res, next ) => {
     // res.send('Login route is working!');
     const { username, email, password } = req.body;
     const hashedPassword = bcrypt.hashSync(password, 10);
@@ -10,6 +11,6 @@ export const login = async (req, res) => {
         await newUser.save();
         res.status(201).json('User created successfully');
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        next(error);
     }
 }
