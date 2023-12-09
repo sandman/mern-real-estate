ddf3fbd40b290afb9f5099aca31b1286115ed0fe
Sandip Gangakhedkar <sandip.gangakhedkar@gmail.com>
2023-12-09 02:20:11 +0000

    Sign up page UI

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold'>Register</h1>
+      <form className='flex flex-col gap-4'>
+        <input type='text' placeholder='Username'
+        className='border p-3 rounded-lg' id='username' />
+        <input type='email' placeholder='Email'
+        className='border p-3 rounded-lg' id='email' />
+        <input type='password' placeholder='Password'
+        className='border p-3 rounded-lg' id='password' />
+        <button type='submit' className='bg-slate-700 text-white p-3 rounded-lg hover:opacity-95'>Register</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
+        <Link to='/login' className='text-slate-700 hover:underline'>
+          <span className='text-blue-700'>Login</span>
+        </Link>
+      </div>
+    </div>
   )
 }
