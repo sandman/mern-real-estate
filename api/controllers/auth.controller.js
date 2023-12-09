60e7b6e794240e518c3b436cef39f17c948f90cf
Sandip Gangakhedkar <sandip.gangakhedkar@gmail.com>
2023-12-09 05:26:45 +0000

    Adding Register page UI and logic

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,8 +2,8 @@ import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 
-export const login = async (req, res, next ) => {
-    // res.send('Login route is working!');
+export const register = async (req, res, next ) => {
+    // res.send('Register route is working!');
     const { username, email, password } = req.body;
     const hashedPassword = bcrypt.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
@@ -13,4 +13,21 @@ export const login = async (req, res, next ) => {
     } catch (error) {
         next(error);
     }
-}
+};
+
+export const login = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            next(errorHandler(404, 'User not found!'));
+        }
+        const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+        if (!isPasswordCorrect) {
+            next(errorHandler(401, 'Wrong credentials'));
+        }
+        res.status(200).json('User logged in successfully!');
+    } catch (error) {
+        next(error);
+    }
+};
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,18 +1,66 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Register() {
+
+  const [formData, setFormData] = useState({})
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData(
+      { ...formData,
+        [e.target.id]: e.target.value
+      });
+  };
+  console.log(formData)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/register',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success  === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/login');
+    }
+    catch (err) {
+      setLoading(false);
+      setError(err.message);
+    }
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold'>Register</h1>
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit}
+        className='flex flex-col gap-4'>
         <input type='text' placeholder='Username'
-        className='border p-3 rounded-lg' id='username' />
+        className='border p-3 rounded-lg' id='username'
+        onChange={handleChange} />
         <input type='email' placeholder='Email'
-        className='border p-3 rounded-lg' id='email' />
+        className='border p-3 rounded-lg' id='email'
+        onChange={handleChange}/>
         <input type='password' placeholder='Password'
-        className='border p-3 rounded-lg' id='password' />
-        <button type='submit' className='bg-slate-700 text-white p-3 rounded-lg hover:opacity-95'>Register</button>
+        className='border p-3 rounded-lg' id='password'
+        onChange={handleChange} />
+        <button disabled={loading} type='submit'
+          className='bg-slate-700 text-white p-3 rounded-lg hover:opacity-95'>
+            {loading ? 'Loading...' : 'Register' }</button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
@@ -20,6 +68,7 @@ export default function Register() {
           <span className='text-blue-700'>Login</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
